f0608408490120ce0eebd574da72e31f9474a74f
Tiago Segatti <128088339+TigasSegatti@users.noreply.github.com>
2024-06-04 13:55:09 -0300

    Delete SA_Efluentes/projeto-efluentes directory

--- SA_Efluentes/projeto-efluentes/projeto-efluentes/src/main/java/br/com/senai/sa3semestre/projeto_efluentes/entities/TipoEfluente.java
+++
@@ -1,7 +0,0 @@
-package br.com.senai.sa3semestre.projeto_efluentes.entities;
-
-public enum TipoEfluente {
-    QUIMICO,
-    FISICO,
-    BIOLOGICO
-}
--- SA_Efluentes/projeto-efluentes/projeto-efluentes/src/main/java/br/com/senai/sa3semestre/projeto_efluentes/repositories/AmostraRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.senai.sa3semestre.projeto_efluentes.repositories;
-
-import br.com.senai.sa3semestre.projeto_efluentes.entities.Amostra;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AmostraRepository extends JpaRepository<Amostra,Long> {
-}
